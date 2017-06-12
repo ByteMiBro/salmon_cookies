1d8a185a223abd5478e8513cbb67e2cdb83aa2f2
ByteMiBro <bytemibro@gmail.com>
2017-06-12 17:56:22 -0600

    cookeis per hour

--- model.js
+++ model.js
@@ -13,6 +13,7 @@ function randomCount()
 {
   return Math.random() * (this.max - this.min) + this.min;
 };
+// take number of
 function cookiesPurchased(){
   for
   this.simulatedCookiesPerHour = [
@@ -23,10 +24,12 @@ var hoursPerStore{this.hours * numberofStores}
 var firstAndPike = {min:23,
                   max:65,
                   cookiesPerSale:6.3,
-                  //hours: start at 12 am and end at 11 pm please
+                  //hours: specify the actual hours the store is open not the number of hours
+                  //hours: start at the first hour store opens and end with the last hour the store is open
                   hours:[6,7,8,9,10,11,12,1,2,3,4,5,6,7,8]
                   getRandomCustomerCount: randomCount,
-                  generateCustomerCountByHour: cookiesPerHour,
+                  generateCustCountByHour: cookiesPerHour,
+                  cookiesPerHour:
                   };
 
 var seaTacAirport{}
