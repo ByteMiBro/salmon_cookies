b16b4a742e254781f5488206aa3be299781543f1
ByteMiBro <bytemibro@gmail.com>
2017-06-12 17:27:34 -0600

    random customer count

--- model.js
+++ model.js
@@ -4,3 +4,32 @@ var storeModel = [{firstAndPike},
                   {seattleCenter},
                   {capitolHill},
                   {alki}]
+function numberofStores()
+{
+  return storeModel.length
+}
+
+function randomCount()
+{
+  return Math.random() * (this.max - this.min) + this.min;
+};
+function cookiesPurchased(){
+  for
+  this.simulatedCookiesPerHour = [
+    this.getRandomCustomerCount() * this.cookiesPerHour,];
+};
+for
+var hoursPerStore{this.hours * numberofStores}
+var firstAndPike = {min:23,
+                  max:65,
+                  cookiesPerSale:6.3,
+                  //hours: start at 12 am and end at 11 pm please
+                  hours:[6,7,8,9,10,11,12,1,2,3,4,5,6,7,8]
+                  getRandomCustomerCount: randomCount,
+                  generateCustomerCountByHour: cookiesPerHour,
+                  };
+
+var seaTacAirport{}
+var seattleCenter{}
+var capitolHill{}
+var alki{}
