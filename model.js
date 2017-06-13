2e759d4b0639ac684c9fbe0a9c10f7f128245ea8
ByteMiBro <bytemibro@gmail.com>
2017-06-12 20:24:52 -0600

    sea tac airpot properties

--- model.js
+++ model.js
@@ -21,23 +21,37 @@ function cookiesPerHour();
 }
 function cookiesPerDay();
 {
-  return this.generatedCookyCountByHour() * this.hours();
+  return this.generatedCookyCountByHour() * this.hours().length;
 }
-for
+
 //put in if open or if colsed
-var hoursPerStore{this.hours * numberofStores}
+
 var firstAndPike = {min:23,
                   max:65,
                   cookiesPerSale:6.3,
                   //hours: specify the actual hours the store is open not the number of hours
                   //hours: start at the first hour store opens and end with the last hour the store is open
                   hours:[6,7,8,9,10,11,12,1,2,3,4,5,6,7,8]
-                  getRandomCustomerCount: randomCount,
-                  generatedCookyCountByHour: cookiesPerHour,
-                  generatedCookiesByDay: cookiesPerDay
-                  };
+                  getRandomCustomerCount:randomCount,
+                  //firstAndPike.randomCount?
+                  console.log(randomCount);
+                  generatedCookyCountByHour:cookiesPerHour,
+                  console.log(cookiesPerHour);
+                  generatedCookiesByDay:cookiesPerDay
+                  console.log(cookiesPerDay);
+                  }
 
-var seaTacAirport{}
+var seaTacAirport{min:3,
+                  max:24,
+                  cookiesPerSale:1.2,
+                  hours:[6,7,8,9,10,11,12,1,2,3,4,5,6,7,8]
+                  getRandomCustomerCount:randomCount,
+                  console.log(randomCount);
+                  generatedCookyCountByHour:cookiesPerHour,
+                  console.log(cookiesPerHour);
+                  generatedCookiesByDay:cookiesPerDay,
+                  console.log(cookiesPerDay);
+                  }
 var seattleCenter{}
 var capitolHill{}
 var alki{}
