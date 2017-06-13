6aa8bc5680a99f6dc27d93783f84e8e84bcd0cdf
ByteMiBro <bytemibro@gmail.com>
2017-06-13 00:38:46 -0600

    trying to figure out loops

--- model.js
+++ model.js
@@ -4,24 +4,40 @@ var storeModel = [{firstAndPike},
                   {seaTacAirport},
                   {seattleCenter},
                   {capitolHill},
-                  {alki}]
+                  {alki},
+                  ]
 function numberofStores()
 {
-  return storeModel.length
+  return storeModel.length;
 }
 
-function randomCount()
+function randomCount(customersArray)
 {
-  return Math.random() * (this.max - this.min) + this.min;
-};
-// take number of average cookies times
-function cookiesPerHour();
-{
-  return this.getRandomCustomerCount * this.cookiesPerSale;
+  for (var i = 0 ; i < this.hours.length ; i++){
+    Math.random() * (this.max - this.min) + this.min;
+  }
+  return customersArray
+  //store values in array with same number of items as hours
 }
-function cookiesPerDay();
+// take number of average cookies times
+function cookiesPerHour(hourlyCookyArray)
 {
-  return this.generatedCookyCountByHour * this.hours.length;
+  for (var i = 0 ; i < this.hours.length ; i++)
+  {
+    this.cookiesPerSale * this.customersArray[i];
+  }
+  return hourlyCookyArray
+}
+//ceck the amount of hours at every store then multipy that by
+function cookiesPerDay(totalLocationCookies)
+{
+  var calculatedSum = 0;
+  for(i = 0 ; i < this.hours.length ; i++)
+  {
+    var valueFromArray = this.customersArray[i];
+    calculatedSum = sum(valueFromArray, calculatedSum)[0];
+  }
+  return totalLocationCookies;
 }
 
 //put in if open or if colsed
@@ -33,56 +49,66 @@ var firstAndPike = {min:23,
                   //hours: start at the first hour store opens and end with the last hour the store is open
                   hours:[6,7,8,9,10,11,12,1,2,3,4,5,6,7,8],
                   getRandomCustomerCount:randomCount,
+                  customersArray:[]
                   //firstAndPike.randomCount?
-                  console.log(randomCount);
+                  //console.log(randomCount);
                   generatedCookyCountByHour:cookiesPerHour,
-                  console.log(cookiesPerHour);
-                  generatedCookiesByDay:cookiesPerDay
-                  console.log(cookiesPerDay);
+                  hourlyCookyArray:[]
+                  //console.log(cookiesPerHour);
+                  generatedCookiesByDay:cookiesPerDay,
+                  totalLocationCookies:[]
+                  //console.log(cookiesPerDay);
                   }
+                  console.log(firstAndPike.min);
+                  console.log(firstAndPike.max);
+                  console.log(firstAndPike.cookiesPerSale);
+                  console.log(firstAndPike.hours.length);
+                  console.log(firstAndPike.customersArray);
+                  console.log(firstAndPike.hourlyCookyArray);
+                  console.log(firstAndPike.totalLocationCookies);
 
 var seaTacAirport = {min:3,
                   max:24,
                   cookiesPerSale:1.2,
                   hours:[6,7,8,9,10,11,12,1,2,3,4,5,6,7,8],
                   getRandomCustomerCount:randomCount,
-                  console.log(randomCount);
+                  //console.log(randomCount);
                   generatedCookyCountByHour:cookiesPerHour,
-                  console.log(cookiesPerHour);
+                  //console.log(cookiesPerHour);
                   generatedCookiesByDay:cookiesPerDay,
-                  console.log(cookiesPerDay);
+                  // console.log(cookiesPerDay);
                   }
 var seattleCenter = {min:11,
                   max:38,
                   cookiesPerSale:3.7,
                   hours:[6,7,8,9,10,11,12,1,2,3,4,5,6,7,8],
                   getRandomCustomerCount:randomCount,
-                  console.log(randomCount);
+                  //console.log(randomCount);
                   generatedCookyCountByHour:cookiesPerHour,
-                  console.log(cookiesPerHour);
+                  //console.log(cookiesPerHour);
                   generatedCookiesByDay:cookiesPerDay,
-                  console.log(cookiesPerDay);
+                  //console.log(cookiesPerDay);
                   }
 
 var capitolHill = {min:20,
                   max:38,
-                  cookiesPerSale:2.3
+                  cookiesPerSale:2.3,
                   hours:[6,7,8,9,10,11,12,1,2,3,4,5,6,7,8],
                   getRandomCustomerCount:randomCount,
-                  console.log(randomCount);
+                  //console.log(randomCount);
                   generatedCookyCountByHour:cookiesPerHour,
-                  console.log(cookiesPerHour);
+                  //console.log(cookiesPerHour);
                   generatedCookiesByDay:cookiesPerDay,
-                  console.log(cookiesPerDay);
+                  //console.log(cookiesPerDay);
                   }
 var alki = {min:2,
                   max:16,
                   cookiesPerSale:4.6,
                   hours:[6,7,8,9,10,11,12,1,2,3,4,5,6,7,8],
                   getRandomCustomerCount:randomCount,
-                  console.log(randomCount);
+                  //console.log(randomCount);
                   generatedCookyCountByHour:cookiesPerHour,
-                  console.log(cookiesPerHour);
+                  //console.log(cookiesPerHour);
                   generatedCookiesByDay:cookiesPerDay,
-                  console.log(cookiesPerDay);
+                  //console.log(cookiesPerDay);
                   }
--- store.js
+++ store.js
@@ -1,1 +1,3 @@
 //individaual stores become sparate objects
+/*first colum is location second colum is hours third columb is expected customers
+and fouthr colum is expected cookies per hour fifth colum is totol cookies in day*/
