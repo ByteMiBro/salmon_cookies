808051bdaf11503683da244c612f2c6a72ec1aa7
ByteMiBro <bytemibro@gmail.com>
2017-06-14 13:27:04 -0600

    fully functional model

--- model.js
+++ model.js
@@ -1,16 +1,18 @@
-
+'use strict';
 function StoreModel(name, minExpectedCustomers, maxExpectedCustomers, cookiesPerSale, hours)
 {
   this.minExpectedCustomers = minExpectedCustomers;
   this.maxExpectedCustomers = maxExpectedCustomers;
   this.cookiesPerSale = cookiesPerSale;
-  this.hours = [6,7,8,9,10,11,12,13,14,15,16,17,18,19,20];
-  this.generateCustomersByHour = generateRandomCustomersByHour();
-  this.generatedCookieCountByHour = cookiesPerHour();
-  this.generatedCookiesByDay = cookiesPerDay();
+  this.hours = hours;
+  this.generateRandomCustomersByHour();
+  this.cookiesPerHour();
+  this.cookiesPerDay();
 
 };
-
+/*StoreModel.prototype.hours = function(){
+  [6,7,8,9,10,11,12,13,14,15,16,17,18,19,20];
+};*/
 StoreModel.prototype.generateRandomCustomersByHour = function()
 {
 /*  var hour = this.hours[i];
@@ -21,7 +23,7 @@ StoreModel.prototype.generateRandomCustomersByHour = function()
   var randomCustomersPerHour = [];
 
   for (var i = 0 ; i < this.hours.length ; i++) {
-    // TODO: We should not have fractional customers
+    // : We should not have fractional customers
     randomCustomersPerHour[i] = Math.random() * (this.maxExpectedCustomers - this.minExpectedCustomers) + this.minExpectedCustomers;
   }
 
@@ -55,14 +57,28 @@ StoreModel.prototype.cookiesPerDay = function ()
     calculatedSum = (valueFromArray + calculatedSum);
   }
   this.simulatedCookiesByDay = calculatedSum;
-};
+} ;
+
+var firstAndPike = new StoreModel('First And Pike', 23, 65, 6.3, [6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]);
+
+var seaTacAirport = new StoreModel('SeaTac Airport', 3, 24, 1.2, [6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]);
+
+var seattleCenter = new StoreModel('Seattle Center', 11, 38, 3.7, [6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]);
+
+var capitolHill = new StoreModel('Capitol Hill', 11, 38, 2.3, [6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]);
+
+var alki = new StoreModel('Alki', 2, 16, 4.6, [6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]);
 
 var listOfStores = [
-  new StoreModel('First And Pike', 23, 65, 6.3),
-  new StoreModel('SeaTac Airport', 3, 24, 1.2),
-  new StoreModel('Seattle Center', 11, 38, 3.7),
-  new StoreModel('Capitol Hill', 11, 38, 2.3),
-  new StoreModel('Alki', 2, 16, 4.6),
+  new StoreModel('First And Pike', 23, 65, 6.3, [6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]),
+  new StoreModel('SeaTac Airport', 3, 24, 1.2, [6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]),
+  new StoreModel('Seattle Center', 11, 38, 3.7, [6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]),
+  new StoreModel('Capitol Hill', 11, 38, 2.3, [6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]),
+  new StoreModel('Alki', 2, 16, 4.6, [6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]),
 ];
-
+console.log(firstAndPike);
+console.log(seaTacAirport);
+console.log(seattleCenter);
+console.log(capitolHill);
+console.log(alki);
 console.log(listOfStores);
