fb074b3a496bf607f96458a2e8f66165b734f08e
ByteMiBro <bytemibro@gmail.com>
2017-06-14 13:33:14 -0600

    github check

--- store.js
+++ store.js
@@ -1,3 +1,4 @@
 //individaual stores become sparate objects
 /*first colum is location second colum is hours third columb is expected customers
 and fouthr colum is expected cookies per hour fifth colum is totol cookies in day*/
+//ok
