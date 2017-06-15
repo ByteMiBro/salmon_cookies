a492fa05748d48a427d525437c4f7382ce883f86
ByteMiBro <bytemibro@gmail.com>
2017-06-15 16:28:28 -0600

    write basic layout

--- store.js
+++ store.js
@@ -2,64 +2,22 @@
 //individaual stores become sparate objects
 /*first colum is location second colum is hours third columb is expected customers
 and fouthr colum is expected cookies per hour fifth colum is totol cookies in day*/
-var stores = [
-  {
-    name: 'First And Pike',
-    cookiesPerHour: [10, 19, 8],
-  },
-  {
-    name: 'SeaTac Airport',
-    cookiesPerHour: [12, 15, 25],
-  },
-  {
-    name: 'Seattle Center',
-    cookiesPerHour: [10, 19, 8],
-  },
-  {
-    name: 'Capitol Hill',
-    cookiesPerHour: [12, 15, 25],
-  },
-  {
-    name: 'Alki',
-    cookiesPerHour: [12, 15, 25],
-  },
+var listOfStores = [
+  new StoreModel('First And Pike', 23, 65, 6.3, [6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]),
+  new StoreModel('SeaTac Airport', 3, 24, 1.2, [6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]),
+  new StoreModel('Seattle Center', 11, 38, 3.7, [6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]),
+  new StoreModel('Capitol Hill', 11, 38, 2.3, [6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]),
+  new StoreModel('Alki', 2, 16, 4.6, [6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]),
 ];
-console.log(stores);
-
-var storeLocationsContainer = document.getElementById('store-locations');
-console.log(storeLocationsContainer);
-var storeLocationsContainer = document.getElementById('store-locations');
-console.log(storeLocationsContainer);
-var storeLocationsContainer = document.getElementById('store-locations');
-console.log(storeLocationsContainer);
-var storeLocationsContainer = document.getElementById('store-locations');
-console.log(storeLocationsContainer);
-var storeLocationsContainer = document.getElementById('store-locations');
-console.log(storeLocationsContainer);
-for (var i = 0; i < stores.length; i++) {
-  var store = stores[i];
-
-  console.log(store);
-  addStoreRow(storeLocationsContainer, store);
-}
-
-function addStoreRow(container, store) {
-  var storeRow = document.createElement('tr');
-
-  var storeNameCell = document.createElement('td');
-  storeNameCell.textContent = store.name;
-  storeNameCell.setAttribute('colspan', 2);
-  storeRow.appendChild(storeNameCell);
-
-  addCellForEachHour(storeRow, store.avgCustomersByHour);
-
-  container.appendChild(storeRow);
-}
-
-function addCellForEachHour(row, cookiesPerHour) {
-  for (var j = 0; j < cookiesPerHour.length; j++) {
-    var hourCell = document.createElement('td');
-    hourCell.textContent = cookiesPerHour[j];
-    row.appendChild(hourCell);
-  }
-}
+console.log(listOfStores);
+//create a way for each stores array of cookies per hour
+//to be logged in table
+//to do this I need to create a row with the stores name
+//then I need to take the values from that array and log them on a table cell
+//so I need to create a table cell for each item
+for each listOfStores
+var storeRow = document.createElement('tr')
+var storeNameCell = document.createElement('td')
+storeNameCell.textContent = listOfStores.name;
+for each hour collumn
+var document.createElement('td');
