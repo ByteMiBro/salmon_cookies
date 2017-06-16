7d370866ca67ff2fdb5d20f1ce96438db02962f0
ByteMiBro <bytemibro@gmail.com>
2017-06-15 18:02:04 -0600

    add row function

--- store.js
+++ store.js
@@ -1,7 +1,6 @@
 'use strict';
-//individaual stores become sparate objects
-/*first colum is location second colum is hours third columb is expected customers
-and fouthr colum is expected cookies per hour fifth colum is totol cookies in day*/
+//create a way for each stores array of cookies per hour
+//to be logged in table
 var listOfStores = [
   new StoreModel('First And Pike', 23, 65, 6.3, [6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]),
   new StoreModel('SeaTac Airport', 3, 24, 1.2, [6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]),
@@ -10,14 +9,33 @@ var listOfStores = [
   new StoreModel('Alki', 2, 16, 4.6, [6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]),
 ];
 console.log(listOfStores);
-//create a way for each stores array of cookies per hour
-//to be logged in table
+
 //to do this I need to create a row with the stores name
 //then I need to take the values from that array and log them on a table cell
 //so I need to create a table cell for each item
-for each listOfStores
-var storeRow = document.createElement('tr')
-var storeNameCell = document.createElement('td')
-storeNameCell.textContent = listOfStores.name;
-for each hour collumn
-var document.createElement('td');
+//create an empty element for the first column to store the rows
+
+for (var i = 0 ; i < listOfStores.length ; i++)
+{
+//create a variable that stores the items in list of stores
+  var store = listOfStores[i];
+  //for every item run it through a funtion to add a row for every store
+  addStoreRow(store);
+}
+
+function addStoreRow()
+{
+  //create a row in the body of the table
+  var storeRow = document.createElement('tr');
+  var storeRow = document.createTextNode();
+  //create a cell for the store name
+  var storeNameCell = document.createElement('td');
+  //for every cell provide the name of store from list of stores in text
+  storeNameCell.textContent = store.name;
+  //add a store name cell to every store row
+  storeRow.appendChild(storeNameCell);
+  //don't need to append store name because it goes through row
+  addCellForEachHour(storeRow, store.cookiesPerHour);
+}
+//for each hour collumn
+//var document.createElement('td');
