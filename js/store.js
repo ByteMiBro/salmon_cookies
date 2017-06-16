95de57dcc2eb2a4214e4f5ca32af67c531cba364
ByteMiBro <bytemibro@gmail.com>
2017-06-16 17:01:05 -0600

    put files in folders and added wraper

--- js/store.js
+++ js/store.js
@@ -1,15 +1,6 @@
 'use strict';
 //create a way for each stores array of cookies per hour
 //to be logged in table
-var listOfStores = [
-  new StoreModel('First And Pike', 23, 65, 6.3, [6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]),
-  new StoreModel('SeaTac Airport', 3, 24, 1.2, [6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]),
-  new StoreModel('Seattle Center', 11, 38, 3.7, [6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]),
-  new StoreModel('Capitol Hill', 11, 38, 2.3, [6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]),
-  new StoreModel('Alki', 2, 16, 4.6, [6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]),
-];
-console.log(listOfStores);
-
 //to do this I need to create a row with the stores name
 //then I need to take the values from that array and log them on a table cell
 //so I need to create a table cell for each item
@@ -30,6 +21,7 @@ function addStoreRow()
   var storeRow = document.createTextNode();
   //create a cell for the store name
   var storeNameCell = document.createElement('td');
+  var storeNameCell = document.createTextNode();
   //for every cell provide the name of store from list of stores in text
   storeNameCell.textContent = store.name;
   //add a store name cell to every store row
